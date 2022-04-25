0647f774c661f52e2926b3d22679048c45515808
Hexuan Li <lihexuan1@gmail.com>
2022-04-25 16:56:55 -0700

    install express and create server.js file

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-//The following HTML routes should be created:
-    //GET /notes should return the notes.html file.
-    //GET * should return the index.html file.
-//The following API routes should be created:
-    //GET /api/notes should read the db.json file and return all saved notes as JSON.
-    //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+// Dependencies
+
+// Sets up the Express App
+
+// Sets up the Express app to handle data parsing
+
+// HTML Routes
+
+        //GET /notes should return the notes.html file.
+
+        //GET * should return the index.html file.
+// API Routes
+
+        //GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+        //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+// Listener
