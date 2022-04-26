f0066a7ec7ed8497d11f69a56f054cca73e834c5
Hexuan Li <lihexuan1@gmail.com>
2022-04-26 16:23:22 -0700

    test html routes on Insomnia-pass

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
 
 // Sets up the Express App
 const app = express();
@@ -14,11 +15,11 @@ app.use(express.static('public'));
 
         //GET /notes should return the notes.html file.
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'notes.html'));
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
         //GET * should return the index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // API Routes
@@ -26,7 +27,7 @@ app.get('*', (req, res) => {
         //GET /api/notes should read the db.json file and return all saved notes as JSON.
 
         //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-        
+
         //DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
 
