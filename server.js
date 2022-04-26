5274e7a60644baa613770ebfb5c8d22872e79910
Hexuan Li <lihexuan1@gmail.com>
2022-04-25 17:02:56 -0700

    import express and setup app

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Dependencies
+const express = require('express');
+const path = require('path');
 
 // Sets up the Express App
-
+const app = express();
+const PORT = 3001;
 // Sets up the Express app to handle data parsing
-
+app.use(express.static('public'));
 // HTML Routes
 
         //GET /notes should return the notes.html file.
@@ -16,3 +19,4 @@
         //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 // Listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
