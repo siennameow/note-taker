07aafec8c9049dbf7854a87443eba342c953396a
Hexuan Li <lihexuan1@gmail.com>
2022-04-25 19:25:43 -0700

    add html routes

--- server.js
+++ server.js
@@ -4,19 +4,31 @@ const path = require('path');
 
 // Sets up the Express App
 const app = express();
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 // Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static('public'));
 // HTML Routes
 
         //GET /notes should return the notes.html file.
-
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'notes.html'));
+});
         //GET * should return the index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // API Routes
 
         //GET /api/notes should read the db.json file and return all saved notes as JSON.
 
         //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+        
+        //DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+
 
 // Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
